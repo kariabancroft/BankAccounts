d264113a2345dfa1567ad7941703e0c142a82df6
Kari Bancroft <kariabancroft@gmail.com>
2017-02-22 09:10:33 -0800

    Update initialize to raise argument error on balance

--- lib/account.rb
+++ lib/account.rb
@@ -2,6 +2,8 @@ module Bank
   class Account
     attr_reader :id, :balance
     def initialize(id, balance)
+      raise ArgumentError.new("balance must be >= 0") if balance < 0
+
       @id = id
       @balance = balance
     end
